4c4cd8d1d2a0a25f280a816e9f5b6cce9887d8aa
Your Name <you@example.com>
2022-12-19 00:10:31 +0330

    Complete cards.

---
+++ src/Components/MovieCardDesign.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CardStyle from "../Styles/CardStyle";
+import { generalPosterPath } from "../API/Constant";
+// import Genre from "./Genres";
+import { CircularProgress } from "@mui/material";
+
+export default function MovieCardDesign(props) {
+  return (
+    <CardStyle>
+      <div className="posterNameMovie">
+        <img
+          src={props.poster ? generalPosterPath + props.poster : "noPoster"}
+          alt="Movie Poster"
+        />
+
+        <h4>
+          {" "}
+          {props.title} ({props.year})
+        </h4>
+      </div>
+
+      <div className="overview">
+        <p>{props.overview}</p>
+
+        <div className="rating">
+          <CircularProgress value={props.rating} text={props.rating + "/10"} />
+        </div>
+      </div>
+    </CardStyle>
+  );
+}

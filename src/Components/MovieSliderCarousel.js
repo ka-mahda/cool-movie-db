2e17b6ca311b9e2157677c137f20fba3ee3e0bfd
Your Name <you@example.com>
2022-12-19 00:11:23 +0330

    complete movie slide carousel.

---
+++ src/Components/MovieSliderCarousel.js
@@ -0,0 +1,107 @@
+import React from "react";
+import MovieCardDesign from "./MovieCardDesign";
+import useFetch from "../API/FetchUrl";
+import { CircularProgress, Alert } from "@mui/material";
+
+import SwiperCore, { Pagination,Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+import MovieSliderCarouselStyle from "../Styles/MovieSliderCarouselStyle";
+
+
+
+
+SwiperCore.use([Navigation, Pagination]);
+
+export default function MovieSliderCarousel(props) {
+  //Fetching Data
+  const { data, loading, hasError } = useFetch(props.url);
+  let availibilityDataRecieved = !loading && !hasError && !!data;
+
+  if (loading) return <CircularProgress />;
+  if (hasError) return <Alert severity="hasError">{hasError}</Alert>;
+
+  return (
+    <MovieSliderCarouselStyle>
+      <h3 className="titleSlider">{props.title}</h3>
+      <section className="swiper-container">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          slidesPerView={4}
+          spaceBetween={0}
+          loop={true}
+          navigation={true}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          hashNavigation={{
+            watchState: true,
+          }}
+          className="mySwiper"
+        >
+          {availibilityDataRecieved &&
+            data.results.map((movie) => {
+              return (
+                <>
+                  <SwiperSlide key={movie.id} className="swiperSlide">
+  
+                {" "}
+                <MovieCardDesign
+                  title={movie.title}
+                  year={movie.release_date.slice(0,4)}
+                  overview={movie.overview}
+                  rating={movie.vote_average}
+                  poster={movie.poster_path}
+                  genre={movie.genre_ids[0]}
+                  id={movie.id}
+                  category="movies"
+                  group="movie"
+                /> 
+              </SwiperSlide>
+                </>
+              );
+            })}
+          {/* <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide> */}
+        </Swiper>
+      </section>
+    </MovieSliderCarouselStyle>
+  );
+}
+
+
+  
+
+
+// import * as React from 'react';
+// import Radio from '@mui/material/Radio';
+// import RadioGroup from '@mui/material/RadioGroup';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import FormControl from '@mui/material/FormControl';
+// import FormLabel from '@mui/material/FormLabel';
+
+// export default function ControlledRadioButtonsGroup() {
+//   const [value, setValue] = React.useState('movie');
+
+//   const handleChange = (event) => {
+//     setValue(event.target.value);
+//   };
+
+//   return (
+//     <FormControl>
+//       <RadioGroup
+//         aria-labelledby="demo-controlled-radio-buttons-group"
+//         name="controlled-radio-buttons-group"
+//         value={value}
+//         onChange={handleChange}
+//       >
+//         <FormControlLabel value="movie" control={<Radio />} label="movie" />
+//         <FormControlLabel value="tv" control={<Radio />} label="tv" />
+//       </RadioGroup>
+//     </FormControl>
+//   );
+// }

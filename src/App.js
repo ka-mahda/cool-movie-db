420f04646accdeea83c728a6d32fe755cefcd336
Your Name <you@example.com>
2023-03-01 21:38:45 +0330

    add detail page and actor page to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
-import ReactDOM from 'react-dom';
 import Mainpage from "./SitePages/MainPage";
 import GlobalStyle from "./Styles/GlobalStyle";
-import { Link } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {getLatestMovie} from "./Components/Navbars";
-import { useEffect } from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import ResultOfSearch from "./SitePages/SearchPage";
+import DetailMoviePage  from "./SitePages/MoviePage";
+import DetailTVPage  from "./SitePages/TVPage";
+import ActorBiography from "./Components/ActorBiography";
 
 function App() {
-  // useEffect(()=>{
-        
-  //   getLatestMovie().then((data) => {
-  //     console.log(data)
-      
-  //   });
-   
-  // }, []);
 
   return (
     <>
@@ -28,8 +18,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Mainpage />}></Route>
-          {/* <Route path="/" element={<Search />}></Route> */}
           <Route path="/search/:result" element={<ResultOfSearch />} />
+          <Route path="/movie/:id" element={<DetailMoviePage />} />
+          <Route path="/tv/:id" element={<DetailTVPage />} />
+          <Route path="/person/:id" element={<ActorBiography />} />
          </Routes>
        </BrowserRouter>
        <Footer/>

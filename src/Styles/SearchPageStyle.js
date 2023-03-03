8b025f01a150ab46e70da0e518e2dbe2d7ed0b24
Your Name <you@example.com>
2023-03-03 22:44:20 +0330

    responsive search page

--- src/Styles/SearchPageStyle.js
+++ src/Styles/SearchPageStyle.js
@@ -29,5 +29,25 @@ let SearchPageStyle = styled.div`
     justify-content: space-evenly;
     margin-top: 48px;
   }
+
+  @media only screen and (max-width: 768px) {
+    .searchContainer .leftSideSearchContainer, .searchContainer .rightSideSearchContainer{
+     display:none;
+    }
+    .searchContainer .middleSearchContainer{
+      width:100%;
+    }
+  }
+  @media only screen and (max-width: 448px) {
+    .searchContainer{
+      height:200px;
+    }
+    .searchContainer .middleSearchContainer{
+      margin:0 1rem;
+    }
+    .searchContainer .middleSearchContainer h2, .searchContainer .middleSearchContainer h2 span{
+      font-size: 0.75rem;
+    }
+  }
 `;
 export default SearchPageStyle;

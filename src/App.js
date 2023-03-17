bc987982a6302f29f8aa2300a0620630e97bfeab
Your Name <you@example.com>
2023-03-18 01:43:00 +0330

    Add route of movie and tv genre search page.

--- src/App.js
+++ src/App.js
@@ -8,20 +8,25 @@ import ResultOfSearch from "./SitePages/SearchPage";
 import DetailMoviePage from "./SitePages/MoviePage";
 import DetailTVPage from "./SitePages/TVPage";
 import ActorBiography from "./Components/ActorBiography";
+import MovieGenres from "./SitePages/MovieGenresPage"
+import TvGenres from "./SitePages/TvGenresPage"
 import { ThemeContext } from "./Theme";
 
 function App() {
   const { theme } = useContext(ThemeContext);
   return (
     <div className={`App ${theme}`}>
-      <Header />
+     
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path="/" element={<Mainpage />}></Route>
           <Route path="/search/:result" element={<ResultOfSearch />} />
           <Route path="/movie/:id" element={<DetailMoviePage />} />
           <Route path="/tv/:id" element={<DetailTVPage />} />
           <Route path="/person/:id" element={<ActorBiography />} />
+          <Route path="/movieGenres/:result" element={<MovieGenres />} />
+          <Route path="/tvGenres/:result" element={<TvGenres />} />
         </Routes>
       </BrowserRouter>
       <Footer />
